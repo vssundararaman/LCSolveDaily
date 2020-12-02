0f7ee9091783f2e643a74ad44e207e9f151857d6
vssundararaman@gmail.com <Greatest123$>
2020-12-01 22:48:24 -0600

    code updated

---
+++ src/com/vidya/leetcode/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,49 @@
+package com.vidya.leetcode;
+
+/**
+ * 121. Best Time to Buy and Sell Stock
+ * <p>
+ * Say you have an array for which the ith element is the price of a given stock on day i.
+ * <p>
+ * If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+ * <p>
+ * Note that you cannot sell a stock before you buy one.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [7,1,5,3,6,4]
+ * Output: 5
+ * Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+ * Not 7-1 = 6, as selling price needs to be larger than buying price.
+ * Example 2:
+ * <p>
+ * Input: [7,6,4,3,1]
+ * Output: 0
+ * Explanation: In this case, no transaction is done, i.e. max profit = 0.
+ */
+public class BestTimeToBuyAndSellStock {
+
+    public static void main(String[] args) {
+
+        int[] prices = {7,1,5,3,6,4};
+
+        System.out.println(maxProfit(prices));
+    }
+
+    static int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                max = Math.max(max, prices[i] - min);
+            }
+        }
+        return max;
+    }
+}
